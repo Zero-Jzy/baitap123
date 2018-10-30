9d4ade43c70239bf4c965cf1fc9e0292b9bbe2af
duong phu <dekujzy@gmail.com>
2018-10-30 08:18:41 +0700

    baitapaudio

---
+++ untitled13/myjs.js
@@ -0,0 +1,27 @@
+var youtube_API = "https://2-dot-backup-server-002.appspot.com/_api/v2/songs/get-free-songs"
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        var jsOject = JSON.parse(xhttp.responseText);
+        var imgAudios='';
+        for (var i = 0; i < jsOject.length; i++) {
+            var img='    <div class="column">\n' +
+                '           <img onclick="playAudio(\''+jsOject[i].link+'\')" src="' + jsOject[i].thumbnail + '"  class="hover-shadow cursor">\n' +
+                '    </div>';
+            imgAudios+=img;
+        }
+        document.getElementById('img').innerHTML=imgAudios;
+
+    }
+};
+xhttp.open("GET", youtube_API, true);
+xhttp.send();
+function playAudio(link) {
+    document.getElementById('source').src=link;
+    document.getElementById('audio').play();
+
+}
+
+// document.getElementById('close').onclick=function () {
+//     document.getElementById('audio').src='';
+// };
